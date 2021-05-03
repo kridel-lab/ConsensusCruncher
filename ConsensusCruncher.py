97ff7e97ba00c277ce3ccc219d0c1fe13ac19e54
Gabrielle Persad <82057654+gabrielle-persad@users.noreply.github.com>
2021-05-03 18:09:34 -0400

    change argument to tmp

--- ConsensusCruncher.py
+++ ConsensusCruncher.py
@@ -433,7 +433,7 @@ if __name__ == '__main__':
     sub_a.add_argument('-s', '--samtools', metavar="SAMTOOLS", default =samtools_help,help=samtools_help, type=str)
     sub_a.add_argument('-p', '--bpattern', metavar="PATTERN",  type=str, help=bpattern_help)
     sub_a.add_argument('-l', '--blist', metavar="LIST",  type=str, help=blist_help)
-    sub_b.add_argument('-t', '--temp', type=str)
+    sub_b.add_argument('-tmp', '--temp', type=str)
     sub_a.set_defaults(func=fastq2bam)
 
     # Set args for 'consensus' mode
@@ -447,7 +447,7 @@ if __name__ == '__main__':
                                                     "same base to form a consensus).")
     sub_b.add_argument('-d', '--bdelim', metavar="DELIMITER", default ='|', type=str, help=bdelim_help)
     sub_b.add_argument('--cleanup', choices=['True', 'False'], help=cleanup_help) # Make default
-    sub_b.add_argument('-t', '--temp', type=str)
+    sub_b.add_argument('-tmp', '--temp', type=str)
     sub_b.set_defaults(func=consensus)
 
     # Parse args
